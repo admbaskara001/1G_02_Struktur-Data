8c446fdb8f619f3e2adbfbff2cae87befbe6675a
admbaskara001 <adhambaskara2@gmail.com>
2024-02-20 08:25:04 +0700

    P2_Percobaan1_Pertanyaan4

--- JOBSHEET 2/Buku02.java
+++ JOBSHEET 2/Buku02.java
@@ -1,24 +1,28 @@
 public class Buku02 {
-        String judul, pengarang;
-        int halaman, stok, harga;
+    String judul, pengarang;
+    int halaman, stok, harga;
 
-        void tampilInformasi() {
-            System.out.println("Judul: " + judul);
-            System.out.println("Pengarang: " + pengarang);
-            System.out.println("Jumlah halaman: " + halaman);
-            System.out.println("Sisa stok: " + stok);
-            System.out.println("Harga: Rp " + harga);
-        }
+    void tampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah halaman: " + halaman);
+        System.out.println("Sisa stok: " + stok);
+        System.out.println("Harga: Rp " + harga);
+    }
 
-        void terjual(int jml) {
+    void terjual(int jml) {
+        if (stok > 0) {
             stok -= jml;
-        }
-
-        void restock(int jml) {
-            stok += jml;
-        }
-
-        void gantiHarga(int hrg) {
-            harga = hrg;
+        } else {
+            System.out.println("Stok habis, tidak dapat menjual buku.");
         }
     }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+}
